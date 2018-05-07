d089f85e2991657a4c5d5fe26b28f99f61ac9755
Jakub Najgebauer <jakub.najgebauer@esky.com>
2018-05-07 16:53:04 +0200

    base added

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import face_recognition
 
-# Load the jpg files into numpy arrays
-biden_image = face_recognition.load_image_file("biden.jpg")
-obama_image = face_recognition.load_image_file("obama.jpg")
-unknown_image = face_recognition.load_image_file("obama2.jpg")
+# ladujemy zdjecia
+biden_image = face_recognition.load_image_file("base/1.jpg")
+obama_image = face_recognition.load_image_file("base/2.jpg")
+unknown_image = face_recognition.load_image_file("1.jpg")
 
-# Get the face encodings for each face in each image file
-# Since there could be more than one face in each image, it returns a list of encodings.
-# But since I know each image only has one face, I only care about the first encoding in each image, so I grab index 0.
+# odkodowanie jesli wiecej twarzy na zdjeciu zwraca tablice ale przyjmujemy ze tylko jedna twarz i index 0 bierzemy 1
 try:
     biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
     obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
@@ -21,9 +19,7 @@ known_faces = [
     obama_face_encoding
 ]
 
-# results is an array of True/False telling if the unknown face matched anyone in the known_faces array
+# tablica wynikowa true jesli znana false jesli nie
 results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
 
-print("Is the unknown face a picture of Biden? {}".format(results[0]))
-print("Is the unknown face a picture of Obama? {}".format(results[1]))
-print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))
+print("Czy osoba znana? {}".format(not not True in results))
